a204312025152def7970e8427222da0601fa0a7b
Derrick Camerino <derrick@pagerduty.com>
2016-11-28 13:52:01 -0800

    Adding drag and drop integration test helpers

---
+++ test-support/helpers/ember-drag-drop.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+import $ from 'jquery';
+import Test from 'ember-test';
+import MockDataTransfer from '../../tests/helpers/data-transfer';
+
+function drop($dragHandle, dropCssPath, dragEvent) {
+  let $dropTarget = $(dropCssPath);
+
+  if (dropTargets.length === 0) {
+    throw(`There are no drop targets by the given selector: '${dropCssPath}'`);
+  }
+
+  Ember.run(() => {
+    triggerEvent($dropTarget, 'dragover', MockDataTransfer.makeMockEvent());
+  });
+
+  Ember.run(() => {
+    triggerEvent($dropTarget, 'drop', MockDataTransfer.makeMockEvent(dragEvent.dataTransfer.get('data.payload')));
+  });
+
+  Ember.run(() => {
+    triggerEvent($module, 'dragend', MockDataTransfer.makeMockEvent());
+  });
+}
+
+export default function() {
+  Test.registerAsyncHelper('drag', function(app, cssPath, options={}) {
+    let dragEvent = MockDataTransfer.makeMockEvent();
+    let $dragHandle = $(cssPath);
+
+    Ember.run(() => {
+      triggerEvent($dragHandle, 'mouseover');
+    });
+
+    Ember.run(() => {
+      triggerEvent($dragHandle, 'dragstart', dragEvent);
+    });
+
+    andThen(function() {
+      if (options.beforeDrop) {
+        options.beforeDrop.call();
+      }
+    });
+
+    andThen(function() {
+      if (options.drop) {
+        drop($dragHandle, options.drop, dragEvent);
+      }
+    });
+  });
+}
