b5a9d27c923bed4c7de62e4d7223069f64773839
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2022-11-15 10:50:15 +0100

    chore: move template lint error to a warning

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   extends: 'recommended',
+
+  overrides: [
+    {
+      files: ['./app/templates/components/sortable-objects.hbs'],
+      rules: {
+        'no-yield-only': 'warn',
+      },
+    },
+  ],
 };
