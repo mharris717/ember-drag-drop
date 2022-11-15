a5f622110dcefad3a5a4b39f310415eade896ce1
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2022-11-15 15:55:39 +0100

    chore: temporarily move eslint errors to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,5 +49,71 @@ module.exports = {
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
     },
+    // TODO delete when files are compliant (refactored to native class / glimmer)
+    {
+      files: ['addon-test-support/helpers/data-transfer.js'],
+      rules: {
+        'ember/no-classic-classes': 1,
+      },
+    },
+    {
+      files: ['addon/components/draggable-object-target.js'],
+      rules: {
+        'ember/no-actions-hash': 1,
+        'ember/no-classic-classes': 1,
+        'ember/no-classic-components': 1,
+        'ember/no-component-lifecycle-hooks': 1,
+        'ember/no-get': 1,
+        'ember/no-mixins': 1,
+        'ember/require-tagless-components': 1,
+      },
+    },
+    {
+      files: ['addon/components/draggable-object.js'],
+      rules: {
+        'ember/no-actions-hash': 1,
+        'ember/no-classic-classes': 1,
+        'ember/no-classic-components': 1,
+        'ember/no-component-lifecycle-hooks': 1,
+        'ember/no-get': 1,
+        'ember/no-incorrect-calls-with-inline-anonymous-functions': 1,
+        'ember/require-tagless-components': 1,
+      },
+    },
+    {
+      files: ['addon/components/sortable-objects.js'],
+      rules: {
+        'ember/no-classic-classes': 1,
+        'ember/no-classic-components': 1,
+        'ember/no-component-lifecycle-hooks': 1,
+        'ember/require-tagless-components': 1,
+      },
+    },
+    {
+      files: ['addon/mixins/droppable.js'],
+      rules: {
+        'ember/no-get': 1,
+        'ember/no-new-mixins': 1,
+      },
+    },
+    {
+      files: ['addon/services/drag-coordinator.js'],
+      rules: {
+        'ember/no-classic-classes': 1,
+        'ember/no-get': 1,
+      },
+    },
+    {
+      files: ['app/models/coordinator.js'],
+      rules: {
+        'ember/no-classic-classes': 1,
+      },
+    },
+    {
+      files: ['app/models/obj-hash.js'],
+      rules: {
+        'ember/no-classic-classes': 1,
+      },
+    },
   ],
 };
