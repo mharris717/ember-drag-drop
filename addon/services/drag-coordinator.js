1c2cae4ba287e2f910ab65c792e57b347be7be81
kesavan-ss <83690915+kesavan-ss@users.noreply.github.com>
2023-02-24 10:06:31 +0530

    Remove scope check as we don't need scope check. And this scope checks that it is not working properly when drag drops from one list to another list.

--- addon/services/drag-coordinator.js
+++ addon/services/drag-coordinator.js
@@ -102,7 +102,7 @@ export default Service.extend({
     this.set('lastEvent', event);
 
     if (!this.get('isMoving') && this.get('currentDragEvent')) {
-      if (event.target !== this.get('currentDragEvent').target && hasSameSortingScope) { //if not dragging over self
+      if (event.target !== this.get('currentDragEvent').target) { //if not dragging over self
         if (currentOffsetItem !== emberObject) {
           if (pos.py < 0.67 && moveDirections.indexOf('up') >= 0 ||
               pos.py > 0.33 && moveDirections.indexOf('down') >= 0 ||
